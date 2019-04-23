286265786288578c0277feb237291fe059dcbb0f
colbyktang <colby.us2012@gmail.com>
2019-04-23 13:44:48 -0500

    Practice with DataFrame

---
+++ Python/assignment10.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no','yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data, index=labels)
+#df.info()
+print (df.head(3))
+print ()
+print (df[['animal', 'age']])
+
+
+#print (pd.__version__)
+#pd.show_versions()
--- Python/pandasDataFrame.py
+++ Python/pandasDataFrame.py
@@ -12,7 +12,6 @@ data = {
 data = pd.read_csv("nba.csv", index_col="Name")
 
 first = data["Age"]
-
 print (first)
 
 
